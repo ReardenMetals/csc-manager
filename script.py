b3d068fa537cc9a1e46f78956e2755fc52e9896b
Argon <kartesiur@gmail.com>
2021-01-02 23:32:55 +0300

    Load from config

--- script.py
+++ script.py
@@ -11,7 +11,7 @@ import json
 
 
 # for monero wallet
-#from moneropy import account
+# from moneropy import account
 
 class DashPublicKey(BitcoinPrivateKey):
     _pubkeyhash_version_byte = 0x4c
@@ -20,8 +20,10 @@ class DashPublicKey(BitcoinPrivateKey):
 class ClubCoinPublicKey(BitcoinPrivateKey):
     _pubkeyhash_version_byte = 0x1c
 
+
 class PotCoinPublicKey(BitcoinPrivateKey):
     _pubkeyhash_version_byte = 0x37
+
 
 class CryptoCoin:
     address = ''
@@ -36,6 +38,7 @@ class CryptoCoin:
     def __repr__(self):
         return "address='{}', wif='{}', seed='{}'".format(self.address, self.wif, self.seed)
 
+
 def GenerateBTC():
     private_key = BitcoinPrivateKey.from_passphrase()
     wif = private_key.to_wif()
@@ -45,9 +48,11 @@ def GenerateBTC():
     # line = "{},{},{}\n".format(coin.wif, coin.address, coin.seed)
     return coin
 
+
 def FormatBTC(coin):
     line = "{},{},{}\n".format(coin.wif, coin.address, coin.seed)
     return line
+
 
 def GenerateBCH():
     private_key = BitcoinPrivateKey.from_passphrase()
@@ -58,9 +63,11 @@ def GenerateBCH():
     coin = CryptoCoin(address.replace('bitcoincash:', ''), wif)
     return coin
 
+
 def FormatBCH(coin):
     line = "{},{}\n".format(coin.wif, coin.address)
     return line
+
 
 def GenerateLTC():
     private_key = LitecoinPrivateKey.from_passphrase()
@@ -70,9 +77,11 @@ def GenerateLTC():
     coin = CryptoCoin(address, wif)
     return coin
 
+
 def FormatLTC(coin):
     line = "{},{}\n".format(coin.wif, coin.address)
     return line
+
 
 def GenerateDASH():
     private_key = DashPublicKey.from_passphrase()
@@ -82,9 +91,11 @@ def GenerateDASH():
     coin = CryptoCoin(address, wif)
     return coin
 
+
 def FormatDASH(coin):
     line = "{},{}\n".format(coin.wif, coin.address)
     return line
+
 
 def GenerateCLUB():
     private_key = ClubCoinPublicKey.from_passphrase()
@@ -94,9 +105,11 @@ def GenerateCLUB():
     coin = CryptoCoin(address, wif)
     return coin
 
+
 def FormatCLUB(coin):
     line = "{},{}\n".format(coin.wif, coin.address)
     return line
+
 
 def GeneratePOTE():
     private_key = PotCoinPublicKey.from_passphrase()
@@ -106,9 +119,11 @@ def GeneratePOTE():
     coin = CryptoCoin(address, wif)
     return coin
 
+
 def FormatPOTE(coin):
     line = "{},{}\n".format(coin.wif, coin.address)
     return line
+
 
 def GenerateETH():
     keccak = sha3.keccak_256()
@@ -117,14 +132,16 @@ def GenerateETH():
     pub = priv.get_verifying_key().to_string()
     keccak.update(pub)
     address = keccak.hexdigest()[24:]
-    priv_hex = codecs.encode(priv.to_string(),'hex')
+    priv_hex = codecs.encode(priv.to_string(), 'hex')
     coin = CryptoCoin("0x{}".format(address), priv_hex)
     # line = "{},0x{}\n".format(priv_hex, address)
     return coin
 
+
 def FormatETH(coin):
     line = "{},{}\n".format(coin.wif, coin.address)
     return line
+
 
 # TODO Add this coins
 # def GenerateXMR():
@@ -144,7 +161,8 @@ def toNumber(input):
         print "Please input valid integer! {}".format(input)
         return None
 
-def saveCoinsList(coin, coin_list = [], filename ='address.csv'):
+
+def saveCoinsList(coin, coin_list=[], filename='address.csv'):
     options = {
         "BTC": FormatBTC,
         "BCH": FormatBCH,
@@ -174,26 +192,43 @@ def saveCoinsList(coin, coin_list = [], filename ='address.csv'):
         file.close()
         print "Done!"
 
-def GenerateAssetIdFromBTC(coin_item):
+
+def GenerateAssetIdFromDefault(coin_item):
     coin_address = coin_item.address
-    return coin_address[0:6]
+    return coin_address[1:7]
+
+def GenerateAssetIdFromETH(coin_item):
+    coin_address = coin_item.address
+    return coin_address[2:8]
 
 
-def saveAssetIds(coin, coin_list = [], filename ='assetid.txt'):
+def GenerateAssetId(coin):
     options = {
-        "BTC": GenerateAssetIdFromBTC,
+        "BTC": GenerateAssetIdFromDefault,
+        "ETH": GenerateAssetIdFromETH,
+        "BCH": GenerateAssetIdFromDefault,
+        "LTC": GenerateAssetIdFromDefault,
+        "DASH": GenerateAssetIdFromDefault,
+        'CLUB': GenerateAssetIdFromDefault,
+        # 'XMR': GenerateAssetIdFromDefault,
+        # 'WAVES': GenerateAssetIdFromDefault,
+        'POTE': GenerateAssetIdFromDefault,
     }
+    return options[coin]
 
+
+def saveAssetIds(coin, coin_list=[], filename='assetid.txt'):
     with open(filename, 'w') as file:
         for coin_item in coin_list:
-            line = "{}\n".format(options[coin](coin_item))
+            line = "{}\n".format(GenerateAssetId(coin)(coin_item))
             file.write(line)
             print "Asset IDs {}/{}".format(coin_list.index(coin_item), len(coin_list))
         file.flush()
         file.close()
         print "Done!"
 
-def savePrivateKeys(coin, coin_list = [], filename ='private.txt'):
+
+def savePrivateKeys(coin, coin_list=[], filename='private.txt'):
     with open(filename, 'w') as file:
         for coin_item in coin_list:
             line = "{}\n".format(coin_item.wif)
@@ -203,18 +238,33 @@ def savePrivateKeys(coin, coin_list = [], filename ='private.txt'):
         file.close()
         print "Done!"
 
-def savePublicKeys(coin, coin_list = [], filename ='public.txt'):
-    options = {
-        "BTC": GenerateAssetIdFromBTC,
-    }
+
+def savePublicKeys(coin, coin_list=[], filename='public.txt'):
     with open(filename, 'w') as file:
         for coin_item in coin_list:
-            line = "{},{}\n".format(coin_item.address, options[coin](coin_item))
+            line = "{},{}\n".format(coin_item.address, GenerateAssetId(coin)(coin_item))
             file.write(line)
             print "PublicKeys {}/{}".format(coin_list.index(coin_item), len(coin_list))
         file.flush()
         file.close()
         print "Done!"
+
+def saveSequenseCoinId(lazer_type, coin_list=[], filename = 'sequence.txt'):
+    with open(filename, 'w') as file:
+        for coin_item in coin_list:
+            line = "{}{}\n".format(lazer_type,coin_list.index(coin_item).__format__('04'))
+            file.write(line)
+            print "CoinId Lazer Type {}, Number {}".format(lazer_type,coin_list.index(coin_item).__format__('04'))
+        file.flush()
+        file.close()
+        print "Done!"
+
+def default_input( message, defaultVal ):
+    if defaultVal:
+        return raw_input( "%s [%s] : " % (message,defaultVal) ) or defaultVal
+    else:
+        return raw_input( "%s " % (message) )
+
 
 def init():
     with open('config.json') as json_file:
@@ -224,15 +274,18 @@ def init():
         private_file_name = config_json['private_file_name']
         public_file_name = config_json['public_file_name']
         serial_file_name = config_json['serial_file_name']
+        sequence_file_name = config_json['sequence_file_name']
+
     max_iterator_count = 10
-
     coin = 'BTC'
-
-    output_directory = './'
+    lazer_type = 'A'
+    max_iterator_count = int(default_input("How many codes you want? ","10"))
+    coin = default_input("What crypto you making (BTC, ETH,)? ","BTC").upper()
+    lazer_type = default_input("What laser is this (A, B, C)? ", "A").upper()
 
     if len(argv) >= 4:
         max_iterator_count = toNumber(argv[1])
-        output_directory = argv[2]
+
         coin = argv[3]
     else:
         print "Script will run with default configuration"
@@ -266,6 +319,7 @@ def init():
             saveAssetIds(coin, coin_list, asset_id_file_name)
             savePrivateKeys(coin, coin_list, private_file_name)
             savePublicKeys(coin, coin_list, public_file_name)
+            saveSequenseCoinId(lazer_type,coin_list, sequence_file_name)
         else:
             print "Iterator count should be > 0"
     else:
