e4cfdaeaf8f00a47c02c337eef0ca8599e04618b
bohdan.shlikhutka <bohdan.shlikhutka@gmail.com>
2021-02-22 15:23:35 +0200

    Support compressed & uncompressed keys for LTC & DASH.

--- keygen/currencies/dash_crypto_coin_service.py
+++ keygen/currencies/dash_crypto_coin_service.py
@@ -1,8 +1,13 @@
+from bip_utils.utils import CryptoUtils
+
 from keygen.crypto_coin import CryptoCoin
 from keygen.crypto_coin_service import CoinService
-from bip_utils import Bip39MnemonicGenerator, Bip39SeedGenerator, Bip44, Bip44Coins, Bip44Changes, WifDecoder, DashConf
+from bip_utils import Bip39MnemonicGenerator, Bip39SeedGenerator, Bip44, Bip44Coins, Bip44Changes, WifDecoder, DashConf, \
+    Base58Encoder
 import json
 import subprocess
+
+from keygen.wif_validator import is_compressed_wif
 
 
 class DashCoinService(CoinService):
@@ -24,10 +29,29 @@ class DashCoinService(CoinService):
         return CryptoCoin(address, wif, seed)
 
     def get_coin(self, private_key):
+        if is_compressed_wif(private_key):
+            return self.get_default_coin(private_key)
+        else:
+            return self.get_uncompressed_coin(private_key)
+
+    @staticmethod
+    def get_default_coin(private_key):
         decoded_wif = WifDecoder.Decode(wif_str=private_key,
                                         net_addr_ver=DashConf.WIF_NET_VER.Main())
         key_pair = Bip44.FromAddressPrivKey(decoded_wif, Bip44Coins.DASH)
         address = key_pair.PublicKey().ToAddress()
+        return CryptoCoin(address, private_key)
+
+    @staticmethod
+    def get_uncompressed_coin(private_key):
+        print("Warning Uncompressed key")
+        config_alias = DashConf
+        coin_type = Bip44Coins.DASH
+        decodedWif = WifDecoder.Decode(wif_str=private_key, net_addr_ver=config_alias.WIF_NET_VER.Main())
+        bip44_mst = Bip44.FromAddressPrivKey(decodedWif, coin_type)
+        to_hex = bip44_mst.PublicKey().RawUncompressed().ToBytes()
+        pub_key_bytes = b'\x04' + to_hex
+        address = Base58Encoder.CheckEncode(config_alias.P2PKH_NET_VER.Main() + CryptoUtils.Hash160(pub_key_bytes))
         return CryptoCoin(address, private_key)
 
     # Deprecated
--- keygen/currencies/ltc_crypto_coin_service.py
+++ keygen/currencies/ltc_crypto_coin_service.py
@@ -1,9 +1,13 @@
+from bip_utils.utils import CryptoUtils
+
 from keygen.crypto_coin import CryptoCoin
 from keygen.crypto_coin_service import CoinService
 from bip_utils import Bip39MnemonicGenerator, Bip39SeedGenerator, Bip44, Bip44Coins, Bip44Changes, WifDecoder, \
-    LitecoinConf
+    LitecoinConf, Base58Encoder
 import json
 import subprocess
+
+from keygen.wif_validator import is_compressed_wif
 
 
 class LtcCoinService(CoinService):
@@ -25,10 +29,29 @@ class LtcCoinService(CoinService):
         return CryptoCoin(address, wif, seed)
 
     def get_coin(self, private_key):
+        if is_compressed_wif(private_key):
+            return self.get_default_coin(private_key)
+        else:
+            return self.get_uncompressed_coin(private_key)
+
+    @staticmethod
+    def get_default_coin(private_key):
         decoded_wif = WifDecoder.Decode(wif_str=private_key,
                                         net_addr_ver=LitecoinConf.WIF_NET_VER.Main())
         key_pair = Bip44.FromAddressPrivKey(decoded_wif, Bip44Coins.LITECOIN)
         address = key_pair.PublicKey().ToAddress()
+        return CryptoCoin(address, private_key)
+
+    @staticmethod
+    def get_uncompressed_coin(private_key):
+        print("Warning Uncompressed key")
+        config_alias = LitecoinConf
+        coin_type = Bip44Coins.LITECOIN
+        decodedWif = WifDecoder.Decode(wif_str=private_key, net_addr_ver=config_alias.WIF_NET_VER.Main())
+        bip44_mst = Bip44.FromAddressPrivKey(decodedWif, coin_type)
+        to_hex = bip44_mst.PublicKey().RawUncompressed().ToBytes()
+        pub_key_bytes = b'\x04' + to_hex
+        address = Base58Encoder.CheckEncode(config_alias.P2PKH_NET_VER.Main() + CryptoUtils.Hash160(pub_key_bytes))
         return CryptoCoin(address, private_key)
 
     # Deprecated
